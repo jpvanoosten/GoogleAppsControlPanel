2990488c3a18c08557d9a7e6f2ae2b7e80ebe699
Jeremiah <oosten.j@nhtv.nl>
2014-05-22 12:01:03 +0200

    Adding GoogleAppsControlPanel PHP project.

--- config_template.php
+++ config_template.php
@@ -5,5 +5,3 @@ define("PROJECT_ID","<YOUR_PROJECT_ID>");
 define("CLIENT_ID","<YOUR_CLIENT_ID>");
 define("CLIENT_SECRET","<YOUR_CLIENT_SECRET>");
 define("REDIRECT_URI","<YOUR_REDIRECT_URI>");
-
-?>
