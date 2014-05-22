ca061cbd4c9a639e6a329b3a17a272ee4b9e14fb
Jeremiah van Oosten <jpvanoosten@hotmail.com>
2014-05-22 11:27:56 +0200

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define("PROJECT_NAME","Google Apps Control Panel");
-define("PROJECT_ID","vast-mapper-586");
-define("CLIENT_ID","597486964006-2n4fif2kv6006tv86q965vagdk5mjbur.apps.googleusercontent.com");
-define("CLIENT_SECRET","hFpck7CBRReydxp80xDsNOTa");
-define("REDIRECT_URI","http://gacp.ade-nhtv.nl/oauth2callback");
-
-?>
